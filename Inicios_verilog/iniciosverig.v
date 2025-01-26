a7e6549cd478b2874f2cb92a3b7b16ff18f6383d
BAgaleanoM <bgaleanom@unal.edu.co>
2025-01-25 22:06:19 -0500

    Actualización de bases de Verilog

--- Inicios_verilog/iniciosverig.v
+++ Inicios_verilog/iniciosverig.v
@@ -46,4 +46,21 @@ module mux_con_ifelse(x0, x1, s, f);
 // también se puede usar else if para anidar más condiciones, termina siempre con else
 endmodule
 
+// case, casex, casez y loop for, Va en always, funciona como condicional anidado pero mejor 
+
+module _case(W, S, f); //multiplexor usando case
+  input [0:3] W;
+  input [1:0] S;
+  output reg f;
+
+  always @(W, S)
+    case(S)
+        0: f = W[0];
+        1: f = W[1];
+        2: f = W[2];
+        3: f = W[3];
+        default: f = 0;
+endmodule
+
+//Casez trata todas las z en tos ítems como don't care, Casez
   
