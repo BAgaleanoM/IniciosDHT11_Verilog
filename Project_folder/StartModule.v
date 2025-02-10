e51766f36d560867cb17ac6921d919ed9b0ebb7f
BAgaleanoM <bgaleanom@unal.edu.co>
2025-02-10 01:14:25 -0500

    StartModule Update

--- Project_folder/StartModule.v
+++ Project_folder/StartModule.v
@@ -3,7 +3,7 @@ module StartModule (
     input wire clk,
     input wire rst,
     output reg out_delay,
-    output wire confirm_to_reciver,
+    output reg  confirm_to_reciver;
 );
 
 //Ciclos de envío de datos y recepctión de datos
@@ -16,11 +16,6 @@ reg[2:0] states;
 
 parameter A=0, B=1, C=2, D=3;
 
-initial begin
-    states <= A;
-    out_delay = 1'b0;
-end
-
 //Definición de salidas de la máquina
 always @(states) begin
 
@@ -29,25 +24,61 @@ always @(states) begin
         B: out_delay = 1'b1;
         C: out_delay = 1'b0;
         D: out_delay = 1'b1, confirm_to_reciver = 1'b1;
-        default: out_delay = 1'b1;
+        default: out_delay = 1'b0;
     endcase
 
 end
 
-always @(posedge clk, posedge rst) begin
+initial begin
+    states <= A;
+    out_delay = 1'b0;
+end
+
+always @(posedge clk or posedge rst) begin
     if (rst)
         states <= A;
+        counter = 0;
     else begin
         
-        case (param)
-            : 
-            default: 
+        case (states)
+            A:begin
+                if (counter == send_star_signal) begin
+                    states <= B; // Para pasar al otro estado de la salida
+                    counter = 0; //Para reiniciar el contador a 0
+                end else begin
+                    states <= A;
+                    counter <= counter+1;
+                end
+           end
+            B:begin
+                if (counter == up_to_request) begin
+                    states <= C;
+                    counter = 0;
+                end else begin
+                    states <= B;
+                    counter <= counter+1;
+                end
+            end
+            C:begin
+                if (counter == dht11_response) begin
+                    states <= D;
+                    counter = 0;
+                end else begin
+                    states <= C;
+                    counter <= counter+1;
+                end
+            end
+            D:begin
+                if (counter == dht11_response) begin
+                    states <= D;
+                    counter = 0;
+                end else begin
+                    counter <= counter+1;
+                end
+            end
+            default: counter = 0; 
         endcase
-
-
     end
-
-    
 end
 
 
