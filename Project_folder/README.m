b19faa5180a956df86d5070e5aa56d2bd2f77c1d
BAgaleanoM <bgaleanom@unal.edu.co>
2025-02-23 12:16:26 -0500

    Update README.m

--- Project_folder/README.m
+++ Project_folder/README.m
@@ -1,1 +1,5 @@
+Esta es la carpeta del proyecto, la organización de los módulos es:
 
+El módulo StartModule se encarga de enviar los primeros pulsos al sensro para indicarle que envíe de vuelta los datos, además le indica al módulo DataReciverModule que emperazá a recibir los datos de parte del sensor
+
+El módulo DataReciverModule se encaga de recibir el bus de 40 bits para ser separados en los datos de temperatura y humedad, para luego pasárselos al módulo de la pantalla.
